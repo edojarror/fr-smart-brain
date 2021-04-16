8f1b2dc9bddd438eef46a83f8e7bec92e978dcd6
edojarror <edojarror@gmail.com>
2021-04-16 23:01:43 +0700

    add accept key on header to try fixing CORS problem

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -25,7 +25,9 @@ class Register extends React.Component {
         e.preventDefault(); 
         fetch('https://git.heroku.com/fast-brook-16183.git/register', {
             method: 'post',
-            headers: {"Content-Type": "application/json"},
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/vnd.heroku+json; version=3"},
             body: JSON.stringify({
                 name: this.state.name,
                 email: this.state.email,
